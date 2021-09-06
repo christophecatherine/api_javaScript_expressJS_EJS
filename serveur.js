9aba41728f5ebf33c6af0215900933142b95c459
christophecatherine <1christophecatherine@gmail.com>
2021-09-06 16:10:22 +0200

    post

--- serveur.js
+++ serveur.js
@@ -1,9 +1,13 @@
 // on demande express 
 const express = require("express");
+
 const app = express();
 
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get(['/', '/index'], function(req, res) {
@@ -12,11 +16,19 @@ app.get(['/', '/index'], function(req, res) {
         age: 49
     }
     res.render('index', obj);
-})
+});
 
 app.get('/personnages', function(req, res) {
     res.render('personnages');
 })
+
+app.post(['/', '/index'], function(req, res) {
+    nomRecu = req.body.nom;
+    res.render('index', { nom: nomRecu })
+})
+
+
+
 
 // on ecoute sur le port 9090
 app.listen(9090, function() {
